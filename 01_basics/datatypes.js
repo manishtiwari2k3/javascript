fac8cdb8bc78f366d6e6171ca606c67594a4cdbc
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-08 08:18:11 +0000

    datatypes in js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,45 @@
+// for newer version of javascript
+"use strict";
+// as bohot sara code humara javascript ke older versions mai likha hua tha but due to new versions vo sara code easily usabe ban gaya
+// par javascript nahi chahti thi ki purane code mai koi changes ho toh "use strict" method banaya gya jisse ki hum apne sare code ko
+// newer version k hisab se likh sakte hain
+
+// alert(3+3);
+
+// normally agar hum browser ke andar ya document ke andar is code ko likhte toh 1 pop up aajata
+// but newer versions mai hum kya karte hai ki alert ko likhne ka 1 alag syntax hai normally ye alert ab itna use mai ni hai
+
+// we are using nodejs not browser -> agar browser use karre hote then it is valid
+
+
+// ecma -> standard for writing and defining javascript
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("manish")
+
+
+let name = "manish"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

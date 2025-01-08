f32e3e98e5ad693f92c1b1ecf1f3100992c0d391
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-08 07:46:58 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345;
+let email = "manishtiwari2k3@gmail.com";
+var name1 = "manish";
+id = 1212;
+let m;
+
+console.table([accountId,email,name1,id]);
+
+// accountId = 1212; not allowed
+
+email = "codeblink31@gmail.com" // allowed
+
+name1 = "tiwari"; //allowed
+
+id = 2121;
+
+console.table([accountId,email,name1,id,m]);

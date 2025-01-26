4a07df8c467a0fc589003b11b7ae911475960ab6
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-26 14:46:48 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,33 @@
+// console.log("M")
+// console.log("A")
+// console.log("N")
+// console.log("I")
+// console.log("S")
+// console.log("H")
+
+// functions are basically used to make our work easy
+// functions ke sath sath hum js mai memory management bhi seekhenge
+// memory management is not that much complex in js like c++ etc.
+
+// we can declare function in javascript using the using function keyword
+
+function myName(){
+    console.log("M")
+    console.log("A")
+    console.log("N")
+    console.log("I")
+    console.log("S")
+    console.log("H")
+}
+
+// myname -> this is the reference of the above function
+
+// myName() // this is the calling of the above function
+
+function add2num(num1 , num2){
+    console.log(num1+num2)
+}
+
+// add2num() // this will say not a number NaN
+
+add2num(5 , 7)

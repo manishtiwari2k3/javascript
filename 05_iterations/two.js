6a406e8996a98243d28116bf3c8d261c47f76d28
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2025-01-30 10:35:26 +0000

    while nad do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while and do while loop
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+// do while loop minimum 1 bar toh run hoga hi hoga
+// 1 bar toh hota hi hai run fer check karta hai
+// agar condition satisfy bhi nahi hori tab bhi vo 1 bar toh execute karega hi value ko
+// kuki isme condition last mai check hoti hai
